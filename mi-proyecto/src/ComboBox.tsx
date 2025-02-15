2f8b811b446c2f6a7900e043b4ee319ed201372a
Abdi Guillermo <ext.jaguillermo@maxillc.com>
2025-02-15 15:02:35 -0600

    mejorar

--- mi-proyecto/src/ComboBox.tsx
+++ mi-proyecto/src/ComboBox.tsx
@@ -11,11 +11,25 @@ const ComboBox = () => {
   const [query, setQuery] = useState("");
   const [ShowLoading, setShowLoading] = useState(false);
   const [showCombobox, setshowCombobox] = useState(false);
+  const [selected, setSelected] = useState<string | "">("");
   const [showComboboxNoFound, setshowComboboxNoFound] = useState(false);
 
   const sleep = (milliseconds : any) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
-}
+  }
+  const funncionesCombobox = (result : Opcion[]) =>{
+
+    if (result.length > 0) {
+      setShowLoading(false)
+      setshowCombobox(true)
+      setshowComboboxNoFound(false)
+    }
+    else {
+      setShowLoading(false)
+      setshowCombobox(false)
+      setshowComboboxNoFound(true)
+    }
+  }
   const eventos = async(name: string) => {
     try {
       const response = axios.get<Opcion[]>(`http://localhost:3000/search?name=${name}`);
@@ -27,16 +41,8 @@ const ComboBox = () => {
         await sleep(3000);
         const result = (await response).data
         setOpciones(result);
-        if (result.length > 0) {
-          setShowLoading(false)
-          setshowCombobox(true)
-          setshowComboboxNoFound(false)
-        }
-        else {
-          setShowLoading(false)
-          setshowCombobox(false)
-          setshowComboboxNoFound(true)
-        }
+        funncionesCombobox(result)
+        
 
 
       
@@ -55,11 +61,13 @@ const ComboBox = () => {
       <input
         type="text"
         placeholder="Buscar..."
-        value={query}
+        value={selected}
         onChange={
           (e) => {
+            
             eventos(e.target.value)
-            setQuery(e.target.value)
+            setSelected(e.target.value)
+            
           }
 
         }
@@ -79,7 +87,13 @@ const ComboBox = () => {
       {opciones.length > 0 && showCombobox && (
         <ul id="boxselector" className="absolute left-0 right-0 bg-white border rounded-lg shadow-md mt-1 max-h-40 overflow-y-auto">
           {opciones.map((item, index) => (
-            <div className="combo-row pointer" id={item.id} onClick={(e) => eventos(query)} >
+            <div className="combo-row pointer" id={item.id} onClick={(e) => {
+              setshowCombobox(false)
+              setOpciones([]);
+              setSelected(item.name);
+            }
+              
+            } >
               <div className="combo-column1">
                 <img src={"/src/icons/ic_" + item.id + ".svg"} />
               </div>
